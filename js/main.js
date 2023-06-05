7c2558c30593ed23461905d1a619c447423be272
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2023-06-05 12:33:49 -0700

    Link a mi cv en drive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const button__show_cv = null || document.getElementById("button__show_cv");
 
 button__show_cv.addEventListener('click', () => {
-    window.open("","_blank")
+    window.open("https://drive.google.com/file/d/1zu-KFGEom8-tiVOSGV4R18gTk9k4zqgr/view?usp=sharing","_blank")
 })
